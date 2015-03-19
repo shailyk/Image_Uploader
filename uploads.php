3f931f513d99c37d9504c2286529308488836510
shailyk <shailyk@hotmail.com>
2015-03-19 01:31:31 -0700

    commit

---
+++ uploads.php
@@ -0,0 +1,41 @@
+<?php
+	
+    ini_set('file_uploads', 'On');
+	
+	$target_dir = "uploads/";
+	$target_file = $target_dir . basename($_FILES["fileToUpload"]["name"]);
+	$uploadOk = 1;
+	$imageFileType = pathinfo($target_file,PATHINFO_EXTENSION);
+	// Check if image file is a actual image or fake image
+	if(isset($_POST["submit"])) {
+		$check = getimagesize($_FILES["fileToUpload"]["tmp_name"]);
+		if($check !== false) {
+			//echo "File is an image - " . $check["mime"] . ".";
+			$uploadOk = 1;
+			} else {
+			//echo "File is not an image.";
+			$uploadOk = 0;
+		}
+		
+		if ($uploadOk == 0) {
+			//echo "Sorry, your file was not uploaded.";
+			// if everything is ok, try to upload file
+			} else {
+			if (move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $target_file)) {
+				//echo "The file ".$_FILES["fileToUpload"]["name"]. " has been uploaded.";
+				} else {
+				//echo "Sorry, there was an error uploading your file.";
+			}
+		}
+	}
+
+	header('Content-type: $check["mime"];charset=utf-8');
+	readfile('uploads/'.$_FILES["fileToUpload"]["name"]);
+
+	
+
+	
+	
+	
+	
+?>
