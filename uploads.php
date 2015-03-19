7ed55aa93de2ae45ca4a8bc7cb8356f3b95a1a2a
shailyk <shailyk@uvic.ca>
2015-03-19 01:41:13 -0700

    Update uploads.php

--- uploads.php
+++ uploads.php
@@ -10,21 +10,20 @@
 	if(isset($_POST["submit"])) {
 		$check = getimagesize($_FILES["fileToUpload"]["tmp_name"]);
 		if($check !== false) {
-			//echo "File is an image - " . $check["mime"] . ".";
+			
 			$uploadOk = 1;
 			} else {
-			//echo "File is not an image.";
+		
 			$uploadOk = 0;
 		}
 		
 		if ($uploadOk == 0) {
-			//echo "Sorry, your file was not uploaded.";
-			// if everything is ok, try to upload file
+		
 			} else {
 			if (move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $target_file)) {
-				//echo "The file ".$_FILES["fileToUpload"]["name"]. " has been uploaded.";
+			
 				} else {
-				//echo "Sorry, there was an error uploading your file.";
+			
 			}
 		}
 	}
